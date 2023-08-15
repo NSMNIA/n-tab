19a535bcb25e4c5355263e5c7d307d4fd2b5d1fb
NSMNIA <kevin.stoop@revolveddesign.com>
2023-08-15 21:51:03 +0200

    added search
    Signed-off-by: NSMNIA <kevin.stoop@revolveddesign.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Time from "@/components/Time";
+import Search from "@/components/Search";
 
 const App = () => {
     return (
@@ -6,13 +7,14 @@ const App = () => {
             <div className="absolute h-full w-full top-0 left-0">
                 <div className="h-full w-full bg-[#000] opacity-20 absolute left-0 top-0 select-none pointer-none" />
                 <img
-                    className="h-full w-full object-cover"
+                    className="h-full w-full object-cover "
                     src="/background.webp"
                     alt="Background"
                 />
             </div>
             <div className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] text-center flex flex-col gap-3 p-4">
                 <Time />
+                <Search />
             </div>
         </div>
     );
