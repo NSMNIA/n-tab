481aacf4765154caacc55d426da55c844ea1a3e6
NSMNIA <kevin.stoop@revolveddesign.com>
2023-10-31 17:25:33 +0100

    refactor: removed unused theme
    Signed-off-by: NSMNIA <kevin.stoop@revolveddesign.com>

--- public/theme.js
+++
@@ -1,2 +0,0 @@
-const theme = localStorage.getItem("theme");
-if (theme) document.documentElement.classList.add(`${theme}`);
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "@/styles/globals.css";
-import { ThemeProvider } from "./context/ThemeProvider.tsx";
 import TimeProvider from "./context/TimeProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("NTab")!).render(
     <>
-        <ThemeProvider defaultTheme="system">
-            <TimeProvider>
-                <App />
-            </TimeProvider>
-        </ThemeProvider>
+        <TimeProvider>
+            <App />
+        </TimeProvider>
     </>
 );
